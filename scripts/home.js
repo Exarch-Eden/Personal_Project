613a9066b07fbcaf4228e4f294936bed86dd736c
kurtbot <56173650+kurtbot@users.noreply.github.com>
2019-12-21 01:10:56 -0800

    edited the size for navigation in home.html

--- scripts/home.js
+++ scripts/home.js
@@ -4,7 +4,7 @@ var testHomeData = [
     { title: 'Thread Title', datePosted: 'Posted 7 days ago', threadUser: 'Jon Dork', threadInfo: 'Some thread info here' },
     { title: 'Thread Title', datePosted: 'Posted 7 days ago', threadUser: 'Jon Dork', threadInfo: 'Some thread info here' },
     { title: 'Thread Title', datePosted: 'Posted 7 days ago', threadUser: 'Jon Dork', threadInfo: 'Some thread info here' },
-    { title: 'Thread Title', datePosted: 'Posted 7 days ago', threadUser: 'Jon Dork', threadInfo: 'Some thread info here' },
+    { title: 'Thread Title', datePosted: 'Posted 7 days ago', threadUser: 'K Milan', threadInfo: 'All hope is lost' },
     { title: 'Thread Title', datePosted: 'Posted 7 days ago', threadUser: 'Jon Dork', threadInfo: 'Some thread info here' },
     { title: 'Thread Title', datePosted: 'Posted 7 days ago', threadUser: 'Jon Dork', threadInfo: 'Some thread info here' },
     { title: 'Thread Title', datePosted: 'Posted 7 days ago', threadUser: 'Jon Dork', threadInfo: 'Some thread info here' }
