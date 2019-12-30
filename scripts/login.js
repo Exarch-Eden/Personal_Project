6012579a3c34738f0bcfb0a9422de2c24b536abc
kurtbot <56173650+kurtbot@users.noreply.github.com>
2019-12-29 21:23:38 -0800

    changed login page design

--- scripts/login.js
+++ scripts/login.js
@@ -2,58 +2,104 @@
 //     if (user) {
 //         // User is signed in.
 
-//         document.getElementById("user_div").style.display = "block";
-//         document.getElementById("login_div").style.display = "none";
+//         document.getElementById('user_div').style.display = 'block';
+//         document.getElementById('login_div').style.display = 'none';
 
 //         var user = firebase.auth().currentUser;
 
 //         if (user != null) {
 
 //             var email_id = user.email;
-//             document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+//             document.getElementById('user_para').innerHTML = 'Welcome User : ' + email_id;
 
 //         }
 
 //     } else {
 //         // No user is signed in.
 
-//         document.getElementById("user_div").style.display = "none";
-//         document.getElementById("login_div").style.display = "block";
+//         document.getElementById('user_div').style.display = 'none';
+//         document.getElementById('login_div').style.display = 'block';
 
 //     }
 // });
 
+
 function login() {
 
-    var userEmail = document.getElementById("emailField").value;
-    var userPass = document.getElementById("passwordField").value;
+    var userEmail = document.getElementById('emailField').value;
+    var userPass = document.getElementById('passwordField').value;
 
-    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).then(function() {
-        location.href = 'home.html';
-      }).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-
-        // window.alert("Error : " + errorMessage);
-        document.getElementById("loginHelp").innerText = "Error : " + errorMessage;
-        // ...
-    });
+    if(userEmail != null && userPass != null) {
+        firebase.auth().signInWithEmailAndPassword(userEmail, userPass).then(function () {
+            location.href = 'home.html';
+        }).catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+    
+            // window.alert('Error : ' + errorMessage);
+            // document.getElementById('loginHelp').innerText = 'Error : ' + errorMessage;
+            // ...
+        });
+    }  
 
 }
 
 function signup() {
-    var email = $('#emailSignUp').value;
-    var password = $('#passwordSignUp').value;
+    var email = document.getElementById('emailField').value;
+    var password = document.getElementById('passwordField').value;
 
     firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
         // ...
-        console.log(errorCode, " =>", errorMessage);
+        console.log(errorCode, ' =>', errorMessage);
     });
+
+    console.log('logged in');
+    
 }
+
+function signupShow() {
+    var fields = "<p>Create your account</p>\
+    <input type='text' class='form-control' name='firstNameField' id='firstNameField' aria-describedby='firstNameHelpId' placeholder='first name' required>\
+    <input type='text' class='form-control' name='lastNameField' id='lastNameField' aria-describedby='lastNameHelpId' placeholder='last name' required>\
+    <input type='email' class='form-control' name='emailField' id='emailField' aria-describedby='emailHelpId' placeholder='email' required>\
+    <input type='password' class='form-control' name='passwordField' pattern='[A-Za-z0-9]{8,16}' id='passwordField' aria-describedby='passwordHelpId' placeholder='password' required>\
+    <small id='loginHelp' class='form-text text-muted'></small>\
+    <div>\
+      <button onclick='signup()'>Signup</button>\
+    </div>\
+    <div id='register'>\
+      Already have an account?\
+      <a onclick='loginShow()' href='#login'>Login</a>\
+    </div>"
+
+    var cont = $('#formCont');
+    cont.empty();
+    cont.append(fields);
+}
+
+function loginShow() {
+    var fields = "<p>Welcome back</p>\
+    <input type='email' class='form-control' name='emailField' id='emailField' aria-describedby='emailHelpId' placeholder='email' required>\
+    <input type='password' class='form-control' name='passwordField' pattern='[A-Za-z0-9]{8,16}' id='passwordField' aria-describedby='passwordHelpId' placeholder='password' required>\
+    <small id='loginHelp' class='form-text text-muted'></small>\
+    <div>\
+      <button onclick='login()'>Login</button>\
+    </div>\
+    <div id='register'>\
+      Already have an account?\
+      <a onclick='signupShow()' href='#signup'>Login</a>\
+    </div>"
+
+    var cont = $('#formCont');
+    cont.empty();
+    cont.append(fields);
+}
+
 function logout() {
     firebase.auth().signOut();
 }
+
