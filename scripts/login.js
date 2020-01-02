8c0f782fe6da5b9f1f395594323bbd45f1ac410a
kurtbot <56173650+kurtbot@users.noreply.github.com>
2020-01-02 13:38:30 -0800

    fixed login function not relocating to home.html

--- scripts/login.js
+++ scripts/login.js
@@ -4,15 +4,19 @@ function login() {
     var password = document.getElementById('passwordField').value;
 
     if (email && password) {
-        firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(function () {
+                location.href = 'home.html';
+            })
+            .catch(function (error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
 
-            // window.alert('Error : ' + errorMessage);
-            // document.getElementById('loginHelp').innerText = 'Error : ' + errorMessage;
-            // ...
-        });
+                // window.alert('Error : ' + errorMessage);
+                // document.getElementById('loginHelp').innerText = 'Error : ' + errorMessage;
+                // ...
+            });
     }
 
 }
